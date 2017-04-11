bee45b37b113e1c8748125a05da3b0bae75e9c46
Kim DoKy <makingfunk0@gmail.com>
2017-04-12 05:12:45 +0900

    open_api - DB API view code 리팩토링

--- django_app/content_api/views.py
+++ django_app/content_api/views.py
@@ -1,37 +1,33 @@
-from content_api.models import Content
-from rest_framework import filters
-from rest_framework import viewsets
+from rest_framework import filters, generics
+from rest_framework.pagination import PageNumberPagination
 
-from .serializers import ContentSerializer
+from .models import Content
+from .serializers import ContentDetailSerializer, ContentSimpleSerializer
+
+# Pagination 개별 설정을 위한 클래스
+class DefaultResultsSetPagination(PageNumberPagination):
+    page_size = 6
 
 
-class SnippetViewSet(viewsets.ModelViewSet):
+# Content DB 정보 API
+class ContentViewSet(generics.ListAPIView):
     queryset = Content.objects.all()
-    serializer_class = ContentSerializer
-    filter_backends = (filters.OrderingFilter, filters.SearchFilter)
-    ordering = ('-start_date', '-id')
-    search_fields = ('seq', 'title', 'start_date', 'end_date', 'place', 'realm_name',
-                     'area', 'price', 'phone')
+    filter_backends = (filters.DjangoFilterBackend, filters.OrderingFilter,)
 
+    # 필터링 조건 (카테고리)
+    filter_fields = ('seq', 'area', 'realm_name')
 
-class DetailViewSet(viewsets.ModelViewSet):
-    queryset = Content.objects.all()
-    serializer_class = ContentSerializer
-    filter_backends = (filters.SearchFilter, )
-    search_fields = ('seq', 'title')
+    # 최신순 정렬
+    ordering = ('-start_date', '-seq')
 
+    # 한페이지당 6개의 아이템
+    pagination_class = DefaultResultsSetPagination
 
-class AreaViewSet(viewsets.ModelViewSet):
-    queryset = Content.objects.all()
-    serializer_class = ContentSerializer
-    filter_backends = (filters.OrderingFilter, filters.SearchFilter)
-    ordering = ('-start_date', '-id')
-    search_fields = ('area', 'area')
-
-
-class GenreViewSet(viewsets.ModelViewSet):
-    queryset = Content.objects.all()
-    serializer_class = ContentSerializer
-    filter_backends = (filters.OrderingFilter, filters.SearchFilter)
-    ordering = ('-start_date', '-id')
-    search_fields = ('realm_name', 'realm_name')
+    # 요청 조건에 따라 serializer 선택
+    def get_serializer_class(self):
+        if self.request.query_params.get('seq'):
+            return ContentDetailSerializer
+        elif self.request.query_params.get('realm_name'):
+            return ContentSimpleSerializer
+        elif self.request.query_params.get('area'):
+            return ContentSimpleSerializer
