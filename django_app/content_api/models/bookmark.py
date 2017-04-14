d28df21358af871d388758ddf618b40510906cf1
YM Choi <archoiym@gmail.com>
2017-04-14 12:55:11 +0900

    Bookmark관련 admin 설정.

--- django_app/content_api/models/bookmark.py
+++ django_app/content_api/models/bookmark.py
@@ -14,6 +14,7 @@ class Bookmark(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.ForeignKey(Content, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
+    # description은 추후 프론트팀이 가능하면 작업할 내용 - 최영민
     description = models.CharField(max_length=200)
 
     def __str__(self):
