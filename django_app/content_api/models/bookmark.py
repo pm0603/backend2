8cdd0183a303b9e0521f029ccfd84e24063d1ca7
YM Choi <archoiym@gmail.com>
2017-04-14 11:57:03 +0900

    content_api에서 models패키지를 만들어 리팩토링 및 Bookmark 클래스 구성 시작.

---
+++ django_app/content_api/models/bookmark.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+__all__ = (
+    'Bookmark',
+)
+
+class Bookmark(models.Model):
+    pass
--- django_app/content_api/models/content.py
+++ django_app/content_api/models/content.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+__all__ = (
+    'Content',
+)
 
 class Content(models.Model):
     seq = models.CharField(max_length=20, unique=True)
