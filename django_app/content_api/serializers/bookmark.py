2c6f3f151e32da8b50df23359f7ede23e736e420
YM Choi <archoiym@gmail.com>
2017-04-17 23:15:58 +0900

    수정중.

--- django_app/content_api/serializers/bookmark.py
+++ django_app/content_api/serializers/bookmark.py
@@ -5,12 +5,12 @@ from content_api.models import Content
 
 __all__ = (
     'BookmarkSerializer',
+    'BookmarkedContentSerializer',
 )
 
 
 # user가 북마크한 content에 대한 상세한 정보를 가져오기 위해 추가적으로 설정 - 최영민
 class BookmarkedContentSerializer(serializers.ModelSerializer):
-
 
     class Meta:
         model = Content
@@ -21,10 +21,10 @@ class BookmarkSerializer(serializers.ModelSerializer):
     # username = serializers.ModelField(model_field='user.username')
     # content_title = serializers.Field(source='content.title')
 
-    content = serializers.CharField()
-    # details = BookmarkedContentSerializer(many=True, read_only=True)
-    price = serializers.PrimaryKeyRelatedField(queryset=Content.objects.all())
+    # content = serializers.CharField
+    content = BookmarkedContentSerializer(many=True, read_only=True)
+    #bookmark = BookmarkedContentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Bookmark
-        fields = ('content', 'description', 'price',)
+        fields = ('content', 'user')
--- django_app/member/serializers.py
+++ django_app/member/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.core.paginator import Paginator
 from rest_framework import serializers
 
 from content_api.serializers.bookmark import BookmarkedContentSerializer
@@ -12,6 +13,9 @@ class UserSerializer(serializers.ModelSerializer):
 
     # user가 북마크한 content에 대한 상세한 정보를 가져오기 위해 추가적으로 설정 - 최영민
     bookmarks = BookmarkedContentSerializer(many=True, read_only=True)
+
+    paginator = Paginator(bookmarks, 2)
+    bookmarkss = paginator.page(1).object_list
 
     class Meta:
         model = get_user_model()
