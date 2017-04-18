a4d3b93869dc93f258f8f679c8c7edeeedfeba80
YM Choi <archoiym@gmail.com>
2017-04-18 20:03:13 +0900

    BookmarkSerializer에 몇 가지 필드 추가.

--- django_app/content_api/serializers/bookmark.py
+++ django_app/content_api/serializers/bookmark.py
@@ -23,7 +23,10 @@ class BookmarkSerializer(serializers.ModelSerializer):
     title = serializers.CharField(source='content.title', required=False)
     price = serializers.CharField(source='content.price', required=False)
     start_date = serializers.DateField(source='content.start_date', required=False)
+    end_date = serializers.DateField(source='content.end_date', required=False)
+    area = serializers.CharField(source='content.area', required=False)
+    place = serializers.CharField(source='content.place', required=False)
 
     class Meta:
         model = Bookmark
-        fields = ('content', 'title', 'price', 'start_date')
+        fields = ('content', 'title', 'price', 'area', 'place', 'start_date', 'end_date')
